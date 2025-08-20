e7d81a28de8b99151631af04df74e91227fafdad
Felipe Fidelis Tobias <felipe.fidelistobias11@gmail.com>
2025-08-19 23:40:17 -0300

    The multiples of 10 from 0 to 100

---
+++ Activity7/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 10;
+            for (int i = 0; i <= 100; i++) 
+            {
+                Console.WriteLine(i);
+                if (i % 10 == 0) 
+                {
+                    Console.WriteLine("Multiplo de 10");  
+                }
+            }
+        }
+    }
+}
