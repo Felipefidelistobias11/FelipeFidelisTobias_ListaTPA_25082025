79e483a7603c9495f8fb3ceabba5ca3393fa1ed8
Felipe Fidelis Tobias <felipe.fidelistobias11@gmail.com>
2025-08-19 23:29:33 -0300

    Multiplication table

---
+++ Activity3/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Digite um numero:");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(n + " x " + i + " = " + (n*i));
+            }
+        }
+    }
+}
